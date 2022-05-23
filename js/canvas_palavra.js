9ad4b4a88fec64ada64c5a9b83b563f86d502315
Vinicius Canuto Falcão <viniciusfalcao82@gmail.com>
2022-05-23 13:20:47 -0300

    salvar

---
+++ js/canvas_palavra.js
@@ -0,0 +1,14 @@
+var canvas=document.querySelector('#canvas_palavra')
+var context=canvas.getContext('2d');
+context.fillStyle='dark blue';
+context.fillRect(6+6+4,18,50,25);
+context.fillRect(60+6+4,18,50,25);
+context.fillRect(114+6+4,18,50,25);
+context.fillRect(162+6+6+4,18,50,25);
+context.fillRect(216+6+6+4,18,50,25);
+
+
+
+
+
+
